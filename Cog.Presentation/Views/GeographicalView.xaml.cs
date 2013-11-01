682d52d79aeefc95f9ae9b92b28599a84870bd69
Damien Daspit <damien_daspit@sil.org>
2013-11-01 13:38:12 +0700

    better feedback from map search

--- Cog.Presentation/Views/GeographicalView.xaml.cs
+++ Cog.Presentation/Views/GeographicalView.xaml.cs
@@ -13,6 +13,7 @@ using GMap.NET;
 using GMap.NET.WindowsPresentation;
 using GalaSoft.MvvmLight.Threading;
 using SIL.Cog.Applications.ViewModels;
+using SIL.Cog.Presentation.Services;
 
 namespace SIL.Cog.Presentation.Views
 {
@@ -315,8 +316,20 @@ namespace SIL.Cog.Presentation.Views
 
 			using (BusyCursor.Display())
 			{
-				MapControl.SetPositionByKeywords(SearchTextBox.Text);
-				MapControl.Zoom = 11;
+				GeoCoderStatusCode res = MapControl.SetPositionByKeywords(SearchTextBox.Text);
+				switch (res)
+				{
+					case GeoCoderStatusCode.G_GEO_SUCCESS:
+						MapControl.Zoom = 11;
+						break;
+					case GeoCoderStatusCode.G_GEO_UNKNOWN_ADDRESS:
+					case GeoCoderStatusCode.Unknow:
+						MessageBoxEx.Show(Application.Current.MainWindow, "Location not found.", "Cog", MessageBoxButton.OK, MessageBoxImage.Information);
+						break;
+					case GeoCoderStatusCode.G_GEO_SERVER_ERROR:
+						MessageBoxEx.Show(Application.Current.MainWindow, "Unable to connect to map server.", "Cog", MessageBoxButton.OK, MessageBoxImage.Error);
+						break;
+				}
 			}
 		}
 

26aafcf59d269e42367f1896facf80fecbe45167
Andrew Polk <andrew_polk@sil.org>
2013-11-08 10:35:19 +0700

    Modified Select/Unselect All checkbox to work in three states and respond to the selecting and unselecting of the individual checkboxes.

--- Cog.Applications/ViewModels/SelectVarietiesViewModel.cs
+++ Cog.Applications/ViewModels/SelectVarietiesViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using GalaSoft.MvvmLight;
 using SIL.Cog.Domain;
@@ -9,12 +10,19 @@ namespace SIL.Cog.Applications.ViewModels
 	public class SelectVarietiesViewModel : ViewModelBase
 	{
 		private readonly ReadOnlyList<SelectableVarietyViewModel> _varieties;
-		private bool _selectAll;
+		private bool? _selectAll;
+		private readonly SimpleMonitor _monitor;
 
 		public SelectVarietiesViewModel(IEnumerable<Variety> varieties, ISet<Variety> selectedVarieties)
 		{
 			_varieties = new ReadOnlyList<SelectableVarietyViewModel>(varieties.Select(v => new SelectableVarietyViewModel(v) {IsSelected = selectedVarieties.Contains(v)}).ToArray());
 			_selectAll = selectedVarieties.Count > 0;
+			_monitor = new SimpleMonitor();
+			if (selectedVarieties.Count > 0 && selectedVarieties.Count < _varieties.Count)
+				_selectAll = null;
+
+			foreach (SelectableVarietyViewModel variety in _varieties)
+				variety.PropertyChanged += Varieties_PropertyChanged;
 		}
 
 		public ReadOnlyList<SelectableVarietyViewModel> Varieties
@@ -22,16 +30,47 @@ namespace SIL.Cog.Applications.ViewModels
 			get { return _varieties; }
 		}
 
-		public bool SelectAll
+		public bool? SelectAll
 		{
 			get { return _selectAll; }
 			set
 			{
-				if (Set(() => SelectAll, ref _selectAll, value))
+				using (_monitor.Enter())
 				{
-					foreach (SelectableVarietyViewModel variety in _varieties)
-						variety.IsSelected = _selectAll;
+					if (Set(() => SelectAll, ref _selectAll, value))
+					{
+						foreach (SelectableVarietyViewModel variety in _varieties)
+							variety.IsSelected = _selectAll == null || (bool) _selectAll;
+					}
 				}
+			}
+		}
+
+		private void Varieties_PropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			if (e.PropertyName == "IsSelected" && !_monitor.Busy)
+			{
+				bool hasChecked = false;
+				bool hasUnchecked = false;
+				bool? newValue = null;
+				foreach (SelectableVarietyViewModel variety in _varieties)
+				{
+					if (variety.IsSelected)
+					{
+						hasChecked = true;
+						if (hasUnchecked)
+							break;
+					}
+					else
+					{
+						hasUnchecked = true;
+						if (hasChecked)
+							break;
+					}
+				}
+				if (!hasChecked || !hasUnchecked)
+					newValue = hasChecked;
+				Set(() => SelectAll, ref _selectAll, newValue);
 			}
 		}
 	}

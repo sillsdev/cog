d8c92f3444fe344b8bada6629bacec9672014523
Robin Munn <robin_munn@sil.org>
2015-08-18 09:28:38 +0700

    WIP POC - Ready for testing on Linux
    Now taking input from stdin and writing to stdout, and/or to files specified
    as options on the command line. Ready to do some manual testing on Linux,
    then create a few unit tests.

--- Cog.Application.CommandLine/Program.cs
+++ Cog.Application.CommandLine/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.ComTypes;
 using System.Text;
@@ -7,6 +8,14 @@ using System.Threading.Tasks;
 using CommandLine;
 using SIL.Cog.Domain;
 using SIL.Machine.Annotations;
+
+enum ReturnCodes
+{
+	Okay = 0,
+	InputError,
+	NotImplemented,
+	UnknownVerb
+}
 
 namespace SIL.Cog.Application.CommandLine
 {
@@ -17,44 +26,82 @@ namespace SIL.Cog.Application.CommandLine
 			int retcode = Parser.Default.ParseArguments<SegmentOptions, CompareOptions>(args)
 				.Return(
 					(SegmentOptions opts) => DoSegmentation(opts),
-					(CompareOptions opts) => { Console.WriteLine("Sorry, \"compare\" action not yet implemented."); return 2; },
-					(errs) => 3);
+					(CompareOptions opts) => DoCompare(opts),
+					(errs) => (int)ReturnCodes.UnknownVerb);
 			return retcode;
+		}
+
+		private static StreamReader OpenInput(string filename)
+		{
+			return OpenInput(filename, new UTF8Encoding());
+		}
+
+		private static StreamReader OpenInput(string filename, Encoding encoding)
+		{
+			Stream source = filename == "-" ? Console.OpenStandardInput() : new FileStream(filename, FileMode.Open, FileAccess.Read);
+			return new StreamReader(source, encoding);
+		}
+
+		private static StreamWriter OpenOutput(string filename)
+		{
+			return OpenOutput(filename, new UTF8Encoding());
+		}
+
+		private static StreamWriter OpenOutput(string filename, Encoding encoding)
+		{
+			Stream source = filename == "-" ? Console.OpenStandardOutput() : new FileStream(filename, FileMode.Create, FileAccess.Write);
+			return new StreamWriter(source, encoding);
 		}
 
 		private static int DoSegmentation(SegmentOptions options)
 		{
 			SpanFactory<ShapeNode> spanFactory = new ShapeSpanFactory();
 
-			Console.WriteLine("Input from " + options.InputFilename + " and output to " + options.OutputFilename);
+			StreamReader Input = OpenInput(options.InputFilename);
+			StreamWriter Output = OpenOutput(options.OutputFilename);
 
-			foreach (string word in options.Words)
+			var segmenter = new Segmenter(spanFactory)
 			{
-				Console.WriteLine("Will parse {0}", word);
-				var segmenter = new Segmenter(spanFactory)
-				{
-					Consonants = { "b", "c", "ch", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "sh", "t", "v", "w", "x", "z" },
-					Vowels = { "a", "e", "i", "o", "u" },
-					Boundaries = { "-" },
-					Modifiers = { "\u0303", "\u0308" },
-					Joiners = { "\u0361" }
-				};
+				Consonants = { "b", "c", "ch", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "sh", "t", "v", "w", "x", "z" },
+				Vowels = { "a", "e", "i", "o", "u" },
+				Boundaries = { "-" },
+				Modifiers = { "\u0303", "\u0308" },
+				Joiners = { "\u0361" }
+			};
+
+			int retcode = (int)ReturnCodes.Okay;
+
+			while (!Input.EndOfStream)
+			{
+				string line = Input.ReadLine();
+				if (line == null)
+					break;
+
+				StreamWriter stderr = new StreamWriter(Console.OpenStandardError()); // For demo. Real implementation might allow logging to a file.
+
+				string word = line; // For demo, one word per line. Real implementation might expect something like "word method" (space-separated) on each line.
 
 				Shape shape;
-				bool success = segmenter.TrySegment(word, out shape);
-				if (success)
+				if (segmenter.TrySegment(word, out shape))
 				{
-					Console.WriteLine("Parsed {0}. Results:", word);
-					var nodes = shape.Annotations;
-					Console.WriteLine(nodes.ToString());
+					AnnotationList<ShapeNode> nodes = shape.Annotations;
+					Output.WriteLine(nodes.ToString());
 				}
 				else
 				{
-					Console.WriteLine("Failed to parse {0}", word);
-					return 1;
+					stderr.WriteLine("Failed to parse {0}", word);
+					retcode = (int)ReturnCodes.InputError;
 				}
 			}
-			return 0;
+			Input.Close();
+			Output.Close();
+			return retcode;
+		}
+
+		private static int DoCompare(CompareOptions options)
+		{
+			Console.WriteLine("Sorry, \"compare\" action not yet implemented.");
+			return (int)ReturnCodes.NotImplemented;
 		}
 	}
 }

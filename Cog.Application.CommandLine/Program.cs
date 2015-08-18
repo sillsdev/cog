2c876dbca072aa748eec4d1267579fd1146a886c
Robin Munn <robin_munn@sil.org>
2015-08-18 12:26:38 +0700

    Trim unused "using" directives. Ready for review.

--- Cog.Application.CommandLine/Program.cs
+++ Cog.Application.CommandLine/Program.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.ComTypes;
-using System.Text;
-using System.Threading.Tasks;
-using CommandLine;
-using SIL.Cog.Domain;
-using SIL.Machine.Annotations;
-
+﻿using CommandLine;
 
 namespace SIL.Cog.Application.CommandLine
 {

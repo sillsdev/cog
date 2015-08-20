ac2de8d1fbc662d4ba66d45d44cd6c5b7cafdd43
Robin Munn <robin_munn@sil.org>
2015-08-20 17:01:25 +0700

    First implementation of distance command
    Output score is currently not very useful; what we probably want will be
    a number between 0.0 and 1.0, where 1.0 means "very distant" and 0.0 means
    identical. What we currently have is an integer where the higher the score,
    the closer the words. But this is a good first step towards getting a proper
    distance score later.

---
+++ Cog.Application.CommandLine.Tests/DistanceTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace SIL.Cog.Application.CommandLine.Tests
+{
+	[TestFixture]
+	public class DistanceTests
+	{
+		[Test, Sequential]
+		public void CheckDistance(
+			[Values("chair apple", "apple chair", "chair cheer", "apple apple")] string input,
+			[Values("|chair| |apple| 650", "|apple| |chair| 650", "|chair| |cheer| 16500", "|apple| |apple| 17500")] string expectedOutput)
+		{
+			var inputStream = new StringReader(input);
+			var outputStream = new StringWriter();
+			var distancer = new DistanceVerb();
+			distancer.Method = "Aline";
+			var retcode = distancer.DoWork(inputStream, outputStream);
+			string TextResult = outputStream.ToString().Replace("\r\n", "\n").Replace("\n", "");
+			Assert.That(TextResult, Is.EqualTo(expectedOutput));
+			Assert.That(retcode, Is.EqualTo(0));
+		}
+	}
+}
--- Cog.Application.CommandLine/DistanceVerb.cs
+++ Cog.Application.CommandLine/DistanceVerb.cs
@@ -1,21 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using CommandLine;
+using SIL.Cog.Domain;
+using SIL.Cog.Domain.Components;
+using SIL.Machine.Annotations;
 
 namespace SIL.Cog.Application.CommandLine
 {
 	[Verb("distance", HelpText = "Distance between words (not yet implemented)")]
-	class DistanceVerb : CommonOptions
+	public class DistanceVerb : CommonOptions
 	{
-		[Value(0)]
-		public IList<string> Words { get; set; } // Not actually used for much, but this shows how to get a word list on the command line
+		[Value(0, Default = "Aline", HelpText = "Process name (case-insensitive: e.g., Aline or aline)")]
+		public string Method { get; set; }
+
+		private readonly SpanFactory<ShapeNode> _spanFactory = new ShapeSpanFactory();
+		private Dictionary<string, Word> _parsedWords = new Dictionary<string, Word>();
+
+		protected Word ParseWordOnce(string wordText, Meaning meaning, CogProject project)
+		{
+			// We expect to see a lot of duplicates in our input text; save time by memoizing
+			if (_parsedWords.ContainsKey(wordText))
+				return _parsedWords[wordText];
+			Word word = ParseWord(wordText, meaning);
+			project.Segmenter.Segment(word);
+			_parsedWords.Add(wordText, word);
+			return word;
+		}
 
 		public override int DoWork(TextReader input, TextWriter output)
 		{
-			Console.WriteLine("Sorry, \"distance\" action not yet implemented.");
-			Console.WriteLine("But I did get the list of words you wanted to compare by distance: {0}", String.Join(", ", Words));
-			return (int)ReturnCodes.NotImplemented;
+			int retcode = (int)ReturnCodes.Okay;
+			var segmentPool = new SegmentPool();
+
+			var variety = new Variety("variety1");
+			var meaning = new Meaning("gloss1", "cat1");
+			CogProject project = CommandLineHelpers.GetProject(_spanFactory, segmentPool);
+			project.Meanings.Add(meaning);
+			project.Varieties.Add(variety);
+
+			WordAlignerBase wordAligner;
+			switch (Method.ToLower())
+			{
+				case "aline":
+					wordAligner = (Aline)project.WordAligners["primary"];
+					break;
+
+				default:
+					retcode = (int)ReturnCodes.InputError;
+					return retcode;
+			}
+
+			foreach (string line in input.ReadLines())
+			{
+				string[] wordTexts = line.Split(' ');
+				Word[] words = wordTexts.Select(wordText => ParseWordOnce(wordText, meaning, project)).ToArray();
+				var result = wordAligner.Compute(words[0], words[1]);
+				string[] outWords = words.Select(word => word.ToString().Replace(" ", "")).ToArray();
+				output.WriteLine("{0} {1} {2}", outWords[0], outWords[1], result.BestRawScore);
+			}
+
+			return retcode;
 		}
 	}
 }

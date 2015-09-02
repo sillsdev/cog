3c82c6d8c4ee59ce846db2073bdffb737158b7af
Robin Munn <robin_munn@sil.org>
2015-09-02 09:13:40 +0700

    Reuse global AssemblyInfo instead of recreating it
    This way the version numbering of the command-line component can't get
    out of sync with Cog's version numbering.

--- Cog.Application.CommandLine/Properties/AssemblyInfo.cs
+++ Cog.Application.CommandLine/Properties/AssemblyInfo.cs
@@ -9,11 +9,6 @@ using CommandLine.Text;
 [assembly: AssemblyTitle("SIL.Cog.Application.CommandLine")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SIL")]
-[assembly: AssemblyProduct("Cog")]
-[assembly: AssemblyCopyright("Copyright © 2015 SIL International")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Attributes from CommandLineParser, used in constructing usage help
 [assembly: AssemblyLicense("Code licensed under the MIT License (http://sil.mit-license.org/)")]
@@ -37,5 +32,3 @@ using CommandLine.Text;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]

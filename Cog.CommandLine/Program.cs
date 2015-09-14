32ca2ab97b206e1284936b966604cd7b3218a381
Robin Munn <robin_munn@sil.org>
2015-09-14 16:56:56 +0700

    Refactor: Simplify Program.Main
    Since all verbs inherit from a common VerbBase, there's no need for
    a specific .Return() case for each one, just a single case for anything
    that inherits from VerbBase.

--- Cog.CommandLine/Program.cs
+++ Cog.CommandLine/Program.cs
@@ -15,12 +15,7 @@ namespace SIL.Cog.CommandLine
 		static int Main(string[] args)
 		{
 			ReturnCodes retcode = Parser.Default.ParseArguments<SyllabifyVerb, MakePairsVerb, DistanceVerb, ClusterVerb>(args)
-				.Return(
-					(SyllabifyVerb opts) => opts.RunAsPipe(),
-					(MakePairsVerb opts) => opts.RunAsPipe(),
-					(DistanceVerb opts) => opts.RunAsPipe(),
-					(ClusterVerb opts) => opts.RunAsPipe(),
-					(errs) => ReturnCodes.UnknownVerb);
+				.Return((VerbBase opts) => opts.RunAsPipe(), (errs) => ReturnCodes.UnknownVerb);
 			return (int)retcode;
 		}
 	}

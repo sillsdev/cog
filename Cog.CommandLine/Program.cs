b4644d6dba4e0741cd9e882b79e03f6cd3d4a063
Robin Munn <robin_munn@sil.org>
2015-09-15 08:51:46 +0700

    Refactor: Move ReturnCodes to its own file

--- Cog.CommandLine/Program.cs
+++ Cog.CommandLine/Program.cs
@@ -2,14 +2,6 @@
 
 namespace SIL.Cog.CommandLine
 {
-	public enum ReturnCodes
-	{
-		Okay = 0,
-		InputError = 1,
-		NotImplemented = 2,
-		UnknownVerb = 3
-	}
-
 	class Program
 	{
 		static int Main(string[] args)
---
+++ Cog.CommandLine/ReturnCodes.cs
@@ -0,0 +1,10 @@
+namespace SIL.Cog.CommandLine
+{
+	public enum ReturnCodes
+	{
+		Okay = 0,
+		InputError = 1,
+		NotImplemented = 2,
+		UnknownVerb = 3
+	}
+}

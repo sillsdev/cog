377b650e317909d869e64cc2f26d5cec0ba84a5f
Damien Daspit <damien_daspit@sil.org>
2018-01-18 17:14:32 +0700

    Fix crash when pinning/unpinning cognates

--- Cog.Domain/CognacyDecisionCollection.cs
+++ Cog.Domain/CognacyDecisionCollection.cs
@@ -5,11 +5,11 @@ namespace SIL.Cog.Domain
 {
 	public class CognacyDecisionCollection : ObservableList<CognacyDecision>
 	{
-		private readonly Dictionary<UnorderedTuple<Variety, Variety>, Dictionary<Meaning, int>> _lookupDictionary;
+		private readonly Dictionary<UnorderedTuple<Variety, Variety>, Dictionary<Meaning, CognacyDecision>> _lookupDictionary;
 
 		public CognacyDecisionCollection()
 		{
-			_lookupDictionary = new Dictionary<UnorderedTuple<Variety, Variety>, Dictionary<Meaning, int>>();
+			_lookupDictionary = new Dictionary<UnorderedTuple<Variety, Variety>, Dictionary<Meaning, CognacyDecision>>();
 		}
 
 		public CognacyDecision Add(WordPair wordPair, bool cognacy)
@@ -21,19 +21,13 @@ namespace SIL.Cog.Domain
 
 		public bool Remove(WordPair wordPair)
 		{
-			Dictionary<Meaning, int> decisions;
+			Dictionary<Meaning, CognacyDecision> decisions;
 			UnorderedTuple<Variety, Variety> key = UnorderedTuple.Create(wordPair.VarietyPair.Variety1, wordPair.VarietyPair.Variety2);
 			if (_lookupDictionary.TryGetValue(key, out decisions))
 			{
-				int index;
-				if (decisions.TryGetValue(wordPair.Meaning, out index))
-				{
-					decisions.Remove(wordPair.Meaning);
-					if (decisions.Count == 0)
-						_lookupDictionary.Remove(key);
-					base.RemoveItem(index);
-					return true;
-				}
+				CognacyDecision decision;
+				if (decisions.TryGetValue(wordPair.Meaning, out decision))
+					return Remove(decision);
 			}
 			return false;
 		}
@@ -45,13 +39,13 @@ namespace SIL.Cog.Domain
 
 		public bool? GetCognacy(VarietyPair varietyPair, Meaning meaning)
 		{
-			Dictionary<Meaning, int> decisions;
+			Dictionary<Meaning, CognacyDecision> decisions;
 			if (_lookupDictionary.TryGetValue(UnorderedTuple.Create(varietyPair.Variety1, varietyPair.Variety2),
 				out decisions))
 			{
-				int index;
-				if (decisions.TryGetValue(meaning, out index))
-					return Items[index].Cognacy;
+				CognacyDecision decision;
+				if (decisions.TryGetValue(meaning, out decision))
+					return decision.Cognacy;
 			}
 
 			return null;
@@ -59,7 +53,7 @@ namespace SIL.Cog.Domain
 
 		protected override void InsertItem(int index, CognacyDecision item)
 		{
-			AddToLookupDictionary(index, item);
+			AddToLookupDictionary(item);
 			base.InsertItem(index, item);
 		}
 
@@ -69,18 +63,10 @@ namespace SIL.Cog.Domain
 			base.RemoveItem(index);
 		}
 
-		protected override void MoveItem(int oldIndex, int newIndex)
-		{
-			CognacyDecision item = Items[oldIndex];
-			UnorderedTuple<Variety, Variety> key = UnorderedTuple.Create(item.Variety1, item.Variety2);
-			_lookupDictionary[key][item.Meaning] = newIndex;
-			base.MoveItem(oldIndex, newIndex);
-		}
-
 		protected override void SetItem(int index, CognacyDecision item)
 		{
 			RemoveFromLookupDictionary(index);
-			AddToLookupDictionary(index, item);
+			AddToLookupDictionary(item);
 			base.SetItem(index, item);
 		}
 
@@ -90,18 +76,18 @@ namespace SIL.Cog.Domain
 			base.ClearItems();
 		}
 
-		private void AddToLookupDictionary(int index, CognacyDecision item)
+		private void AddToLookupDictionary(CognacyDecision item)
 		{
-			Dictionary<Meaning, int> lookup = _lookupDictionary.GetValue(UnorderedTuple.Create(item.Variety1, item.Variety2),
-				() => new Dictionary<Meaning, int>());
-			lookup[item.Meaning] = index;
+			Dictionary<Meaning, CognacyDecision> lookup = _lookupDictionary.GetValue(UnorderedTuple.Create(item.Variety1, item.Variety2),
+				() => new Dictionary<Meaning, CognacyDecision>());
+			lookup[item.Meaning] = item;
 		}
 
 		private void RemoveFromLookupDictionary(int index)
 		{
 			CognacyDecision item = Items[index];
 			UnorderedTuple<Variety, Variety> key = UnorderedTuple.Create(item.Variety1, item.Variety2);
-			Dictionary<Meaning, int> decisions = _lookupDictionary[key];
+			Dictionary<Meaning, CognacyDecision> decisions = _lookupDictionary[key];
 			if (decisions.Remove(item.Meaning) && decisions.Count == 0)
 				_lookupDictionary.Remove(key);
 		}

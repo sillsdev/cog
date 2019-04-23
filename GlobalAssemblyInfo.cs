1e00dac829bcf166f70d9d31dc59aa0fed5ea265
Damien Daspit <damien_daspit@sil.org>
2019-04-23 12:18:30 +0700

    Update version to 1.3.3

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -5,10 +5,10 @@
 // associated with an assembly.
 [assembly: AssemblyCompany("SIL")]
 [assembly: AssemblyProduct("Cog")]
-[assembly: AssemblyCopyright("Copyright © 2018 SIL International")]
+[assembly: AssemblyCopyright("Copyright © 2019 SIL International")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 
-[assembly: AssemblyFileVersion("1.3.2.DEV_BUILD")]
-[assembly: AssemblyVersion("1.3.2.0")]
+[assembly: AssemblyFileVersion("1.3.3.DEV_BUILD")]
+[assembly: AssemblyVersion("1.3.3.0")]

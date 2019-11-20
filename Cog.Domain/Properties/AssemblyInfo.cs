dc70529a1c52c1a8057795cb81a49ad3fadd7142
Damien Daspit <damien_daspit@sil.org>
2019-11-20 15:28:51 +0700

    Convert Cog.Domain to .NET Standard

--- Cog.Domain/Properties/AssemblyInfo.cs
+++ Cog.Domain/Properties/AssemblyInfo.cs
@@ -1,19 +1,3 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("SIL.Cog.Domain")]
-[assembly: AssemblyDescription("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e20a24a7-582d-4b61-bebe-4a8b9c2dad3e")]
+﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("SIL.Cog.Domain.Tests")]
